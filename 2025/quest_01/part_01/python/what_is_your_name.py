582332d5e721d18eafe1274ae58d2c03ca932aeb
Jörn Bilicki <hausmarke.github@gmail.com>
2025-11-22 17:31:28 +0100

    Solution of quest 01 / part 1

---
+++ 2025/quest_01/part_01/python/what_is_your_name.py
@@ -0,0 +1,38 @@
+from os import path
+
+workdir = path.abspath(path.dirname(__file__))
+filepath = path.join(workdir, "../everybody_codes_e2025_q01_p1.txt")
+
+NAME_KEY    = 0
+COMMAND_KEY = 1
+
+input_as_map = {}
+
+current_key = NAME_KEY
+
+with open(filepath, 'r') as file:
+    for line in file:
+        if len(line.strip()) > 0:
+            input_as_map[current_key] = line.strip().split(',')
+            current_key += 1
+
+#
+# input_as_map enthält für 
+#   key == 0: Die Liste aller Namen
+#   key == 1: die Liste aller Commands
+# 
+
+name_len = len(input_as_map[NAME_KEY])
+
+current_name_pos = 1
+
+for command in input_as_map[COMMAND_KEY]:
+    direction   = command[:1]
+    shift_count = int(command[1:])
+    match direction:
+        case "R":
+            current_name_pos = min(current_name_pos + shift_count, name_len)
+        case "L":
+            current_name_pos = max(current_name_pos - shift_count, 1)
+
+print(input_as_map[NAME_KEY][current_name_pos-1])
