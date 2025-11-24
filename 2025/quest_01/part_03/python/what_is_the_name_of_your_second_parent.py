09b4a9d7d9156298fd05a9ac2876a30c391fe132
Jörn Bilicki <hausmarke.github@gmail.com>
2025-11-24 14:58:29 +0100

    Solution for quest 01 / part 3

--- 2025/quest_01/part_03/python/what_is_the_name_of_your_second_parent.py
+++ 2025/quest_01/part_03/python/what_is_the_name_of_your_second_parent.py
@@ -8,3 +8,36 @@ import utils
 fname = path.join(workdir, "../everybody_codes_e2025_q01_p3.txt")
 
 input_as_map = utils.prepare_data_from_file(fname)
+
+def get_normalized_shift_count(shift_count, name_len): return shift_count - (shift_count // name_len) * name_len
+
+def swap_names(name_list, old_pos, new_pos): name_list[old_pos-1], name_list[new_pos-1] = name_list[new_pos-1], name_list[old_pos-1]
+
+def r_operation(current_name_pos, shift_count, name_len, name_list):
+    """
+    """
+    normalized_shift_count = get_normalized_shift_count(shift_count, name_len)
+    if normalized_shift_count > 0:
+        # Call the target shift operation
+        new_pos = utils.r_circle_operation(current_name_pos, normalized_shift_count, name_len, name_list)
+        swap_names(name_list, current_name_pos, new_pos)
+    return 1
+
+def l_operation(current_name_pos, shift_count, name_len, name_list):
+    """
+    """
+    normalized_shift_count = get_normalized_shift_count(shift_count, name_len)
+    if normalized_shift_count > 0:
+        # Call the target shift operation
+        new_pos = utils.l_circle_operation(current_name_pos, normalized_shift_count, name_len, name_list)
+        swap_names(name_list, current_name_pos, new_pos)
+    return 1
+
+# Organize the operations in a map with the oparation's shortcut as key.
+operations_map = {
+    "R": r_operation,
+    "L": l_operation
+}
+
+# Prints the resulting name 
+print(utils.get_name(input_as_map, operations_map))
