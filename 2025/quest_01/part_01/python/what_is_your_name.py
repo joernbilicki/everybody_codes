5a7174809d65375ab88c426b55367567b25aa0d2
Jörn Bilicki <hausmarke.github@gmail.com>
2025-11-24 14:58:09 +0100

    Refactoring for solution quest 01 / part 3

--- 2025/quest_01/part_01/python/what_is_your_name.py
+++ 2025/quest_01/part_01/python/what_is_your_name.py
@@ -12,17 +12,17 @@ fname = path.join(workdir, "../everybody_codes_e2025_q01_p1.txt")
 # Prepare a managable structure from the data file
 input_as_map = utils.prepare_data_from_file(fname)
 
-def r_operation(current_name_pos, shift_count, name_len): 
+def r_operation(current_name_pos, shift_count, name_len, name_list): 
     """
     Returns for the "right shift operation" the new position
-    in the name list.
+    in the name list. (ignores the parameter name_list)
     """
     return min(current_name_pos + shift_count, name_len)
 
-def l_operation(current_name_pos, shift_count, name_len):
+def l_operation(current_name_pos, shift_count, name_len, name_list):
     """
     Returns for the "left shift operation" the new position
-    in the name list. (ignores the parameter name_len)
+    in the name list. (ignores the parameters name_len and name_list)
     """
     return max(current_name_pos - shift_count, 1)
 
