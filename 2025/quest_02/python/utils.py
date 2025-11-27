c750f762c4af2b8c1958063c30c6d02b04ed5dad
Jörn Bilicki <hausmarke.github@gmail.com>
2025-11-27 13:15:29 +0100

    Refactor the divide operation due to "rounding effects" with negative numbers

--- 2025/quest_02/python/utils.py
+++ 2025/quest_02/python/utils.py
@@ -12,8 +12,14 @@ def multiply (cn1, cn2):
             cn1[0] * cn2[1] + cn1[1] * cn2[0]]
 
 def divide (cn1, cn2):
-    return [cn1[0] // cn2[0], 
-            cn1[1] // cn2[1]]
+    return [divide_without_rounding(cn1[0],cn2[0]), 
+            divide_without_rounding(cn1[1],cn2[1])]
+
+def divide_without_rounding(v1, v2):
+    result = v1 // v2
+    if v1 < 0 and v1 % v2 > 0:
+        return result + 1
+    return result
 
 def add (cn1, cn2):
     return [cn1[0] + cn2[0], 
