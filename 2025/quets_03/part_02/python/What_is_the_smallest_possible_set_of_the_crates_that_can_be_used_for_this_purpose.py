e8d76e0f9d215910ce5f76b4f9f0d21bd49a5df1
Jörn Bilicki <hausmarke.github@gmail.com>
2025-11-28 16:30:35 +0100

    Solution for quest 03 / part 2

---
+++ 2025/quets_03/part_02/python/What_is_the_smallest_possible_set_of_the_crates_that_can_be_used_for_this_purpose.py
@@ -0,0 +1,20 @@
+from os import path
+import sys
+
+# Make the functionality of a custom file available to import
+workdir = path.abspath(path.dirname(__file__))
+sys.path.insert(1, path.join(workdir, "../../python"))
+import utils
+
+# Create the absolute path to the given data file
+fname = path.join(workdir, "../everybody_codes_e2025_q03_p2.txt")
+
+crate_sizes = utils.prepare_data_from_file(fname)
+
+set_size = 0
+
+for i in range(20):
+    set_size += crate_sizes[i]
+
+print(set_size)
+
