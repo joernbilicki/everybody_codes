66bddbffae69173689e46bab2eb61b743bf1854a
Jörn Bilicki <hausmarke.github@gmail.com>
2025-12-01 20:06:58 +0100

    Solution for quest 06 / part 2

---
+++ 2025/quest_06/part_02/python/solution.py
@@ -0,0 +1,17 @@
+from os import path
+import sys
+
+# Make the functionality of a custom file available to import
+workdir = path.abspath(path.dirname(__file__))
+sys.path.insert(1, path.join(workdir, "../../python"))
+import utils
+
+# Create the absolute path to the given data file
+fname = path.join(workdir, "../everybody_codes_e2025_q06_p2.txt")
+training_plan = utils.prepare_data_from_file(fname)
+
+mentors = "ABC"
+novice_mentor_count = utils.get_count(training_plan, mentors)
+
+# What is the total number of possible novice-mentor pairs?
+print(novice_mentor_count)
