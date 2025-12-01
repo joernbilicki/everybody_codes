13e636e1ea3f557c887d58e1fc1eb111bfbe2978
Jörn Bilicki <hausmarke.github@gmail.com>
2025-12-01 20:02:56 +0100

    New solution for quest 06 / part 1

--- 2025/quest_06/part_01/python/solution.py
+++ 2025/quest_06/part_01/python/solution.py
@@ -10,28 +10,8 @@ import utils
 fname = path.join(workdir, "../everybody_codes_e2025_q06_p1.txt")
 training_plan = utils.prepare_data_from_file(fname)
 
-def add_mentor(mentor_dict:dict, category:str):
-    if category not in mentor_dict: mentor_dict[category] = 0
-    mentor_dict[category] += 1
-
-def add_mentors_for_novice(novice_dict:dict, mentor_dict, category:str):
-    if category not in novice_dict: novice_dict[category] = 0
-    novice_dict[category] += mentor_dict[category.capitalize()]
-
-mentors = "ABC"
-novices = "abc"
-
-mentor_dict = {}
-novice_dict = {}
-
-for category in training_plan:
-    if category in mentors: add_mentor(mentor_dict, category)
-    if category in novices: add_mentors_for_novice(novice_dict, mentor_dict, category)
-
-novice_mentor_count = 0
-
-for novice in novice_dict:
-    print(novice)
+mentors = "A"
+novice_mentor_count = utils.get_count(training_plan, mentors)
 
 # What is the total number of possible novice-mentor pairs?
 print(novice_mentor_count)
--- 2025/quest_06/python/utils.py
+++ 2025/quest_06/python/utils.py
@@ -1,3 +1,58 @@
 def prepare_data_from_file(fname):
+    """
+    Reads the first line of the given data file.
+
+    :param fname: The name of the file.
+    :return: The first line of the file.
+    """
     with open(fname) as file:
         return file.readline()
+
+def add_mentor(mentor_dict:dict, category:str):
+    """
+    Increases the mentor count by one in the given dictionary for the given category (mentor).
+    Creates a new dictionary entry, if there is no entry for the given category.
+
+    :param mentor_dict: Dictionary for mentors.
+    :param category: Category (mentor).
+    """
+    if category not in mentor_dict: mentor_dict[category] = 0
+    mentor_dict[category] += 1
+
+
+def add_mentors_for_novice(novice_dict:dict, mentor_dict, category:str):
+    """
+    Increases the novice count by the amount of the corresponding mentor in the given dictionary for the given category (novice).
+    Creates a new dictionary entry, if there is no entry for the given category.
+
+    :param novice_dict: Dictionary for novices.
+    :param mentor_dict: Dictionary for mentors.
+    :param category: Category (novice).
+    """
+    if category not in novice_dict: novice_dict[category] = 0
+    novice_dict[category] += mentor_dict[category.capitalize()]
+
+
+def get_count(training_plan:str, mentors:str) -> int:
+    """
+    Calculates the count for all novices an its correspüonding mentors.
+
+    :param training_plan: String of novices and mentors.
+    :param mentors: String of all mentors to calculate with. The novice's string will automatically created from this string.
+    :return: Count of possible novice-mentor pairs.
+    """
+    novices = mentors.lower()
+
+    mentor_dict = {}
+    novice_dict = {}
+
+    for category in training_plan:
+        if category in mentors: add_mentor(mentor_dict, category)
+        if category in novices: add_mentors_for_novice(novice_dict, mentor_dict, category)
+
+    novice_mentor_count = 0
+
+    for novice in novice_dict:
+        novice_mentor_count += novice_dict[novice]
+
+    return novice_mentor_count
