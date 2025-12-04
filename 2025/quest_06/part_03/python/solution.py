89e477d1a9f41268911c9a3b05e3cd9a7dc913f6
Jörn Bilicki <hausmarke.github@gmail.com>
2025-12-04 18:13:59 +0100

    Solution for quest 06 / part 3

--- 2025/quest_06/part_03/python/solution.py
+++ 2025/quest_06/part_03/python/solution.py
@@ -10,50 +10,50 @@ import utils
 fname = path.join(workdir, "../everybody_codes_e2025_q06_p3.txt")
 training_plan = utils.prepare_data_from_file(fname)
 
-training_plan = "AABCBABCABCabcabcABCCBAACBCa"
-
-
 
 def count_mentors(working_plan:str, 
                   pos:int, 
-                  training_plan_start_len:int,
-                  training_plan_count:int, 
-                  current_training_plan:int,
+                  training_plan_total_len:int,
                   novice_mentor_distance) -> int:
+    """
+    Counts the mentors for a novice at a given position in the total training plan within a maximal distance.
+    
+    :param working_plan: The total training plan based on the given data and the repetition factor.
+    :type working_plan: str
+    :param pos: The position of the given novice in the working_plan.
+    :type pos: int
+    :param training_plan_total_len: The total length of the working_plan to check the boundary to the right hand side..
+    :type training_plan_total_len: int
+    :param novice_mentor_distance: The maximal distance to find mentors for a given novice (based on its position in the
+    working_plan).
+    :return: The count of mentors found around a novice within the given distance.
+    :rtype: int
+    """
     count = 0
 
     left_end  = pos - novice_mentor_distance
     right_end = pos + novice_mentor_distance + 1
 
-    if current_training_plan == 1: # The position is int the first pattern block
-        if left_end < 0:
-            left_end = 0
-    if current_training_plan == training_plan_count: # The position is in the last pattern block
-        if right_end > training_plan_start_len:
-            right_end = training_plan_start_len
+    if left_end < 0:
+        left_end = 0
+    if right_end > training_plan_total_len:
+        right_end = training_plan_total_len
 
-    count = working_plan[left_end+training_plan_start_len:right_end+training_plan_start_len].count(working_plan[pos].upper())
+    count = working_plan[left_end:right_end].count(working_plan[pos].upper())
     return count
 
-
-working_plan = training_plan * 3 # Only working with the middle part. Avoiding overflow to the left / righ hand side.
-
-novice_mentor_distance = 10
-training_plan_count = 2
-current_training_plan = 1
-
 novices = "abc"
+training_plan_count = 1000
+novice_mentor_distance = 1000
 
-training_plan_start_len = len(training_plan)
-training_plan_total_len = training_plan_start_len * training_plan_count
+working_plan = training_plan * training_plan_count
+training_plan_total_len = len(working_plan)
+
 novice_mentor_count = 0
 
 for i in range(training_plan_total_len):
-    logical_pos = i % training_plan_start_len 
-    if i > 0 and logical_pos == 0:
-        current_training_plan += 1
-    if training_plan[logical_pos] in novices:
-        novice_mentor_count += count_mentors(working_plan, i, training_plan_start_len, training_plan_count, current_training_plan, novice_mentor_distance)
+    if working_plan[i] in novices:
+        novice_mentor_count += count_mentors(working_plan, i, training_plan_total_len, novice_mentor_distance)
 
 # What is the total number of possible novice-mentor pairs?
 print(novice_mentor_count)
