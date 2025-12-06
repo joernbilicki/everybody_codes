cdae4cd4292317376ac0f1288726e724293c51cb
Jörn Bilicki <hausmarke.github@gmail.com>
2025-12-06 19:27:11 +0100

    Solution for quest 07 / part 2

---
+++ 2025/quest_07/part_02/python/solution.py
@@ -0,0 +1,26 @@
+from os import path
+import sys
+
+# Make the functionality of a custom file available to import
+workdir = path.abspath(path.dirname(__file__))
+sys.path.insert(1, path.join(workdir, "../../python"))
+import utils
+
+# Create the absolute path to the given data file
+fname = path.join(workdir, "../everybody_codes_e2025_q07_p2.txt")
+
+# Prepare a managable structure from the data file
+input_as_dict:dict = utils.prepare_data_from_file(fname)
+
+result:int = 0
+
+names:list = input_as_dict[utils.NAMES]
+rules:dict = input_as_dict[utils.RULES]
+
+for i in range(len(names)):
+    name = names[i]
+    if utils.check_name(name, rules):
+        result += (i + 1)
+
+# Find all the names that comply with the rules and calculate the sum of their indices.
+print(result)
