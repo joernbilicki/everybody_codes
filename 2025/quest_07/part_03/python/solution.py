404c0d488140ba1b1b766f35c7c2a6e2c28c0640
Jörn Bilicki <hausmarke.github@gmail.com>
2025-12-15 20:24:08 +0100

    Solution for quest 07 / part 3

--- 2025/quest_07/part_03/python/solution.py
+++ 2025/quest_07/part_03/python/solution.py
@@ -6,23 +6,44 @@ workdir = path.abspath(path.dirname(__file__))
 sys.path.insert(1, path.join(workdir, "../../python"))
 import utils
 
+
+def add_name(name:str, names:set, current_rule_rhs:str, rules:dict) -> None:
+    """
+    Adds a name to a list of unique names. Each name has at least 7 and at most 11 letters.
+    This method walks recursive through a list of letters (based on the rules dictionary) and
+    adds them to the given name.
+    
+    :param name: The name to add to the set of unique names.
+    :type name: str
+    :param names: The resulting set of unique names.
+    :type names: set
+    :param current_rule_rhs: The letters of the righrt hand side of the current rule.
+    :type current_rule_rhs: str
+    :param rules: The rules dictionary.
+    :type rules: dict
+    """
+    if len(name) <= 11: 
+        if 7 <= len(name): 
+            names.add(name)
+        for letter in current_rule_rhs:
+            add_name((name + letter).strip(), names, rules[letter], rules)
+
+
 # Create the absolute path to the given data file
-fname = path.join(workdir, "../everybody_codes_e2025_q07_p3_1.txt")
+fname = path.join(workdir, "../everybody_codes_e2025_q07_p3.txt")
 
 # Prepare a managable structure from the data file
 input_as_dict:dict = utils.prepare_data_from_file(fname)
 
-
 prefixes:list = input_as_dict[utils.NAMES]
 rules:dict    = input_as_dict[utils.RULES]
 
-unique_names:set = {}
+unique_names:set = set()
 
 for prefix in prefixes:
-    final_rule:dict = utils.check_name(prefix, rules)
-    if final_rule != None:
-        # Todo
-        break
+    final_rule_rhs:dict = utils.check_name(prefix, rules)
+    if final_rule_rhs != None:
+        add_name(prefix, unique_names, final_rule_rhs, rules)
 
 # How many unique names can be created based on the given prefixes and rules?
 print(len(unique_names))
